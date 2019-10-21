84ed7f3265fe1ff847a08cc1d666d73afcadc825
Joachim Wickman <joachim@wickman.in>
2019-10-21 11:57:19 +0300

    Initial commit of Jenkins with Android SDK install script.

---
+++ jenkins-android/install-sdk.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl -sSL https://dl.google.com/android/repository/$ANDROID_SDK_FILE | bsdtar -xf - -C $ANDROID_HOME
+chmod +x $ANDROID_HOME/tools/bin/*
+sdkmanager --update
+yes | sdkmanager --licenses
+sdkmanager "build-tools;27.0.1" "platform-tools" "platforms;android-27"
