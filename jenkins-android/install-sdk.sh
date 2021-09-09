d82ea9f65b80ae676933b72fdc3a2271ff869e48
Joachim Wickman <joachim@wickman.in>
2021-09-09 14:11:05 +0300

    Added latest NDK

--- jenkins-android/install-sdk.sh
+++ jenkins-android/install-sdk.sh
@@ -4,4 +4,4 @@ curl -sSL https://dl.google.com/android/repository/$ANDROID_SDK_FILE | bsdtar -x
 chmod +x $ANDROID_HOME/tools/bin/*
 sdkmanager --update
 yes | sdkmanager --licenses
-sdkmanager "build-tools;27.0.1" "platform-tools" "platforms;android-27"
+sdkmanager "build-tools;27.0.1" "platform-tools" "platforms;android-27" "ndk;23.0.7599858"
